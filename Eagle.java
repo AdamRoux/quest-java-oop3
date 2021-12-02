95547fa341aba235da48fa596050729a27d60bb9
AdamRoux <roux.adam69@gmail.com>
2021-12-02 11:56:17 +0100

    quest completed

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,36 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+	@Override
+	public void takeOff() {
+		System.out.println(getName()+" takes off in the sky!");
+		
+	}
+
+	@Override
+	public int ascend(int meters) {
+		System.out.println(getName()+" flies upward, altitude: " + meters);
+		this.altitude = meters;
+		return meters;
+	}
+
+	@Override
+	public int descend(int meters) {
+		System.out.println(getName()+" flies downward, altitude: " + meters);
+		this.altitude = meters;
+		return meters;
+	}
+
+	@Override
+	public void land() {
+		if (this.altitude <= 15) {
+			System.out.println(getName()+" lands on the ground!");
+		}
+		
+		else {
+			System.out.println(getName()+" is too high, it can't land.");
+		}
+		
+	}
 }
